121a223da4626a2fc2174e51aa5eebbbc1231cb1
Ahammed Hinas <82311707+hinasahammed@users.noreply.github.com>
2024-11-26 01:07:55 +0530

    created project

--- lib/main.dart
+++ lib/main.dart
@@ -1,110 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:call_log/call_log.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const CallLogApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class CallLogApp extends StatefulWidget {
+  const CallLogApp({super.key});
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  _CallLogAppState createState() => _CallLogAppState();
+}
+
+class _CallLogAppState extends State<CallLogApp> {
+  List<CallLogEntry> callLogs = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _requestPermission();
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  // Request permission to access call logs
+  void _requestPermission() async {
+    if (await Permission.phone.request().isGranted) {
+      // Permission granted, fetch call logs
+      _getCallLogs();
+    } else {
+      // Show an alert or notify user to grant permission
+      print("Permission denied");
+    }
+  }
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  // Fetch call logs using the call_log package
+  void _getCallLogs() async {
+    Iterable<CallLogEntry> logs = await CallLog.get();
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      callLogs = logs.toList();
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Call Logs')),
+        body: callLogs.isEmpty
+            ? const Center(child: CircularProgressIndicator())
+            : ListView.builder(
+                itemCount: callLogs.length,
+                itemBuilder: (context, index) {
+                  final callLog = callLogs[index];
+                  // Provide a default value of 0 if timestamp is null
+                  final timestamp = callLog.timestamp ?? 0;
+
+                  return ListTile(
+                    title: Text(callLog.name ?? "Unknown"),
+                    subtitle: Text(
+                        'Type: ${callLog.callType}, Date: ${DateTime.fromMillisecondsSinceEpoch(timestamp).toLocal()}'),
+                    leading: Icon(callLog.callType == CallType.incoming
+                        ? Icons.call_received
+                        : Icons.call_made),
+                  );
+                },
+              ),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
